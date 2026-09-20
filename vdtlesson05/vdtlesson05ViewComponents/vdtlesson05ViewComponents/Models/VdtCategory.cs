37f5eb83b0c5384989dcf35daf7241d9a516a550
VDTuan333 <vuduytuan25042006@gmail.com>
2026-09-20 23:16:30 +0700

    view component / partial

---
+++ vdtlesson05/vdtlesson05ViewComponents/vdtlesson05ViewComponents/Models/VdtCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace vdtlesson05ViewComponents.Models
+{
+    public class VdtCategory
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ vdtlesson05/vdtlesson05ViewComponents/vdtlesson05ViewComponents/ViewComponents/VdtCategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using vdtlesson05ViewComponents.Models;
+
+namespace vdtlesson05ViewComponents.ViewComponents
+{
+    public class VdtCategoryViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(bool? active)
+        {
+            var categories =new List<VdtCategory>
+            {
+                new VdtCategory(){CategoryId=1,CategoryName="dien gia dung",IsActive=true},
+                new VdtCategory(){CategoryId=2,CategoryName="iphone",IsActive=true},
+                new VdtCategory(){CategoryId=3,CategoryName="book",IsActive=false},
+                new VdtCategory(){CategoryId=4,CategoryName="laptop",IsActive=true}
+            };
+
+            if(active !=null)
+            {
+                categories = categories.Where(c => c.IsActive == active).ToList();
+            }
+           
+
+            return View(categories);
+        }
+    }
+}
